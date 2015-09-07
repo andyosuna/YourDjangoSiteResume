c6bd29689915cbc25fc7f17816410a71edd149cc
andyosuna <andy.gonzalez@sendoutcards.com>
2015-09-07 17:02:49 -0500

    Added personal information to

--- app/views.py
+++ app/views.py
@@ -8,22 +8,22 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
-    'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'name': 'Andy Gonzalez',
+    'bio': 'Software Engineer',
+    'email': 'info@andyosuna.com',
+    'twitter_username': 'andiosuna',  # No @ symbol, just the handle.
+    'github_username': "andyosuna",
+    'headshot_url': 'https://avatars2.githubusercontent.com/u/3093129',
 }
-    
+
+
 def home(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
         'app/base.html',
-        context_instance = RequestContext(request,
-            {
-                'attendee' : YOUR_INFO,    
-                'year': datetime.now().year,
-            })
+        context_instance=RequestContext(request, {
+            'attendee': YOUR_INFO,
+            'year': datetime.now().year,
+        })
     )
